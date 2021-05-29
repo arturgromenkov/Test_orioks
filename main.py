e9eea8a6583291cef36a7938d1a7410bb48d8f44
arturgromenkov <arturgromenkov@mail.ru>
2021-05-30 00:02:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask,request,current_app,make_response,render_template
+from jinja2 import Template
+app = Flask(__name__,template_folder="D:\\Pycharm\\projects\\template")
+app_context=app.app_context()
+app_context.push()
+def index():
+    return render_template('index.html',name="Jerry")
+app.add_url_rule('/','index',index)
+def books(genre):
+    res = make_response("All Books in {} category".format(genre))
+    res.headers['Content-Type'] = 'text/plain'
+    res.headers['Server'] = 'Foobar'
+    return res
+app.add_url_rule('/books/<genre>','books_and_genre',books)
+
+if __name__ == "__main__":
+    app.run()
