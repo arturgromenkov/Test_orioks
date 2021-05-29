a269ff9651e3fd73c689077ffcd756252b0a4ea4
arturgromenkov <arturgromenkov@mail.ru>
2021-05-30 00:35:10 +0300

    Template_rep was changed

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask,request,current_app,make_response,render_template
 from jinja2 import Template
-app = Flask(__name__,template_folder="D:\\Pycharm\\projects\\template")
+app = Flask(__name__,template_folder="D:\\Pycharm\\Test_orioks\\template")
 app_context=app.app_context()
 app_context.push()
 def index():
-    return render_template('index.html',name="Jerry")
+    return render_template('index.html')
 app.add_url_rule('/','index',index)
 def books(genre):
     res = make_response("All Books in {} category".format(genre))
